5c798c0d1a23f4d5e2056c4d263e078be9bda3c2
felipel7 <felipelseven@gmail.com>
2023-01-24 09:54:49 -0300

    style: create the footer block component

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ collapsible.forEach(item =>
     this.classList.toggle('collapsible--expanded');
   })
 );
+
+const footerMessage = document.querySelector('.footer__copyright');
+const today = new Date();
+
+if (footerMessage) {
+  footerMessage.textContent = `Copyright \u00A9 ${today.getFullYear()} Felipe Silva`;
+}
